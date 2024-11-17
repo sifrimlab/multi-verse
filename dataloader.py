e74a9fb1c0d50341b99a0667f6c4ac139c4de7c1
HannahLy1204 <hanhnguyen12041999@gmail.com>
2024-11-17 19:08:33 +0100

    dataloader upload

--- dataloader.py
+++ dataloader.py
@@ -1,31 +1,207 @@
-import torch
-from torch.utils.data import DataLoader, Dataset
+import json
+import os
+import scanpy as sc
+import anndata as ad
+import mudata as md
+import muon as mu
+from typing import List
+
+#Output type = anndata, mudata, cobolt_data_object?
+
+class DataLoader:
+    def __init__(self, file_path: str = "", modality: str = "", isProcessed=True):
+        self.file_path = file_path
+        self.modality = modality
+        self.is_preprocessed = isProcessed
+        self.data = None
+
+    def read_anndata(self) -> ad.AnnData:
+        """
+        Read files as anndata object
+        Args:
+            Specific modality and file_path must be provided when defining DataLoader object
+            support file format: [".csv", ".tsv", ".h5ad", ".txt", ".mtx", ".h5mu", ".h5"]
+        Returns:
+            A anndata.AnnData object (unprocessed)
+        """
+        adata = None
+        # Modality and file_path should be provided to load anndata object
+        if self.modality != "" and self.file_path != "": 
+            if ".csv" in self.file_path:
+                adata = sc.read_csv(self.file_path)
+            elif ".tsv" in self.file_path:
+                adata = sc.read_csv(self.file_path, sep="\t")
+            elif ".h5ad" in self.file_path:
+                adata = sc.read_h5ad(self.file_path)  
+            elif ".txt" in self.file_path:
+                adata = sc.read_text(self.file_path)
+            elif ".mtx" in self.file_path:
+                mudata = mu.read_10x_mtx(self.file_path) 
+                adata = mudata[self.modality]
+            elif ".h5mu" in self.file_path:
+                mudata = mu.read_h5mu(self.file_path)
+                adata = mudata[self.modality]
+            elif ".h5" in self.file_path:
+                mudata =mu.read_10x_h5(self.file_path)
+                adata = mudata[self.modality]
+
+            if adata:  # Check if adata is not None
+                adata.var_names_make_unique()
+                self.data = adata
+                return self.data
+            else:
+                raise ValueError("Could not read the file. Please check the file path and format.")
+        else:
+            raise ValueError("Modality and file_path must be provided for anndata loading.")
+
+    def read_mudata(self) -> md.MuData:
+        """
+        Read h5mu file as MuData object.
+        Args:
+            file_path must be provided when defining DataLoader object
+            Only support file format ".h5mu", ".h5", ".mtx"
+        Returns:
+            A mudata.MuData object
+        """
+        if self.file_path != "":
+            if ".h5mu" in self.file_path:
+                mudata = mu.read_h5mu(self.file_path)
+            elif ".h5" in self.file_path:
+                mudata =mu.read_10x_h5(self.file_path)
+            elif ".mtx" in self.file_path:
+                mudata = mu.read_10x_mtx(self.file_path)
+            else:
+                raise ValueError("Could not read the file. Only support file format .h5mu, .h5, .mtx.")
+        else:
+            raise ValueError("file_path must be provided to read mudata files")
+        
+        self.data = mudata
+        return self.data
+
+    def fuse_mudata(self, list_anndata: List[ad.AnnData] = None, list_modality: List[str] = None) -> md.MuData:
+        """
+        Fusing paired anndata as MuData
+        intersect_obs will be used if number of obs not equivalent
+        Args:
+            list_modality: A list of strings representing the modalities (e.g., ["rna", "atac", "adt"]).
+            list_anndata: A list of AnnData objects corresponding to the modalities (e.g., [adata_rna, adata_atac, adata_adt]).
+        Returns:s
+            A mudata.MuData object    
+        """
+        if len(list_modality) != len(list_anndata):
+            raise ValueError("Length of list_modality and list_anndata must be equal!")
+
+        data_dict = {}
+        for i, mod in enumerate(list_modality):
+            data_dict[mod] = list_anndata[i]
+
+        self.data = mu.MuData(data_dict)
+        mu.pp.intersect_obs(self.data)   # Make sure number of cells are the same for all modalities
+        return self.data
+
+    def preprocessing(self) -> ad.AnnData:
+        """
+        Preprocessing each anndata object
+        Only support rna, atac, adt modality currently
+        Args:
+            Modality and file_path must be provided when defining DataLoader object
+        Returns:
+            A muon.MuData object
+        """
+         # Modality and file_path should be provided for the read_anndata() function to work
+        if self.modality != "" and self.file_path != "":
+            if not self.is_preprocessed:
+                self.read_anndata()
+                self.data.var_names_make_unique()
+                self.data.layers["counts"] = self.data.X.copy()
+                pre = Preprocessing(self.data)
+                # RNA preprocessing
+                if self.modality=="rna":
+                    self.data = pre.rna_preprocessing()
+                # ATAC preprocessing
+                elif self.modality=="atac":
+                    self.data = pre.atac_preprocessing()
+                # ADT preprocessing
+                elif self.modality=="adt":
+                    self.data = pre.adt_preprocessing()
+                # Not applicable
+                else:
+                    raise ValueError("Preprocessing for this modality is not applicable!")
+        else:
+            raise ValueError("Modality and file_path must be provided to read anndata")
+        return self.data
 
 
-class CustomDataset(Dataset):
-    def __init__(self, data_path, tokenizer, max_length):
-        self.data_path = data_path
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-        self.data = self._load_data()
+class Preprocessing:
+    def __init__(self, anndata: ad.AnnData):
+        self.data = anndata
 
-    def _load_data(self):
-        # Load and preprocess data here
-        return []
+    def rna_preprocessing(self) -> ad.AnnData:
+        """
+        QC metrics for filtering obs depends on specific dataset and experimental condition.
+        Top highly variable gene = 2000
+        Returns:
+            An anndata.AnnData object (proccessed)
+        """
+        # Quality control - based on scanpy calculateQCmetrics - McCarthy et al., 2017
+        self.data.var["mt"] = self.data.var_names.str.startswith("MT-")
+        sc.pp.calculate_qc_metrics(self.data, qc_vars=["mt"], inplace=True, log1p=True)
+        
+        # Filtering -> threshold metrics depend on the specific dataset and experimental conditions.
+        mu.pp.filter_obs(self.data, 'n_genes_by_counts', lambda x: (x >= 500) & (x < 4500))
+        mu.pp.filter_obs(self.data, "total_counts", lambda x: x < 12000)
+        mu.pp.filter_obs(self.data, "pct_counts_mt", lambda x: x < 30)
 
-    def __len__(self):
-        return len(self.data)
+        # Filter genes by keeping only those that are expressed in at least 10 cells.
+        mu.pp.filter_var(self.data, "n_cells_by_counts", lambda x: x >= 10)        
+        
+        # Normalisation
+        sc.pp.normalize_total(self.data, target_sum=1e4)
+        sc.pp.log1p(self.data)
 
-    def __getitem__(self, idx):
-        # Tokenize and return data for training
-        return {}
+        # Feature selection
+        sc.pp.highly_variable_genes(self.data, n_top_genes=2000, subset=True,flavor="seurat")
 
+        return self.data
 
-class DataLoaderFactory:
-    def __init__(self, config):
-        self.config = config
+    def atac_preprocessing(self) -> ad.AnnData:
+        """
+        QC metrics for filtering obs depends on specific dataset and experimental condition.
+        Top highly variable peaks = 15000
+        Returns:
+            An anndata.AnnData object (proccessed)
+        """
+        # Quality control
+        sc.pp.calculate_qc_metrics(self.data, percent_top=None, inplace=True, log1p=False)
 
-    def get_dataloader(self, split):
-        dataset = CustomDataset(
-            self.config['data_path'], self.config['tokenizer'], self.config['max_length'])
-        return DataLoader(dataset, batch_size=self.config['batch_size'], shuffle=split == 'train')
+        # Filter peaks based on number of cells where they are present.
+        mu.pp.filter_var(self.data, "n_cells_by_counts", lambda x: x < 4000)
+        mu.pp.filter_var(self.data, "total_counts", lambda x: x < 1e4)
+
+        # Filter cells based on QC metrics.
+        mu.pp.filter_obs(self.data, "total_counts", lambda x: (x >= 1_000) & (x <= 50_000))
+        mu.pp.filter_obs(self.data, "n_genes_by_counts", lambda x: (x >= 1000) & (x <= 15000))
+
+        # Perform per-cell normalization.
+        sc.pp.normalize_total(self.data, target_sum=1e4)
+        sc.pp.log1p(self.data)
+
+        # Feature selection
+        sc.pp.highly_variable_genes(self.data, n_top_genes=15000, subset=True,flavor="seurat")
+
+        return self.data
+
+    def adt_preprocessing(self) -> ad.AnnData:
+        """
+        Returns:
+            An anndata.AnnData object (proccessed)
+        """
+        # Remove the "total" feature.
+        self.data = self.data[:, 1:]
+        # Make index of proteins compatible with 10X multiome.
+        self.data.obs.index += "-1"
+        # Perform per-cell normalization.
+        mu.prot.pp.clr(self.data)
+        self.data.var["highly_variable"] = True
+
+        return self.data
